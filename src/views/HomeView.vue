0c497c8f838c43daae28925bbb6cd38441b7a098
nellytadi-reo <nellytadi@gmail.com>
2023-02-21 20:18:46 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col" v-if="isVisible">
+        <label>Enter Question</label>
+        <textarea class="form-control"></textarea>
+        <input type="submit" class="btn btn-primary" value="Submit">
+      </div>
+
+      <button @click="toggleQuestion" class="btn btn-dark">Enter New Question</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isVisible: false
+    }
+  },
+  methods: {
+
+    toggleQuestion() {
+      this.isVisible = !this.isVisible;
+    }
+  }
+}
+</script>
